5b7f09038d15d16e6898b18ae3e5e5d9fd969c65
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-07-28 13:40:36 +0300

    tryline and tryboxes

--- src/assets/TryLine.jsx
+++ src/assets/TryLine.jsx
@@ -1,33 +1,15 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+
 import "../App.css";
+import { Trybox } from "./Trybox";
+
 export const TryLine = ({ colours }) => {
-  const [counter, setCounter] = useState(0);
-  const changeColour = () => {
-    if (counter === 5) {
-      setCounter(0);
-    } else {
-      setCounter((prev) => prev + 1);
-    }
-  };
   return (
     <div className="try-line">
-      <div
-        className="square"
-        onClick={changeColour}
-        style={{
-          backgroundColor: colours[counter],
-        }}
-      ></div>
-      <div
-        className="square"
-        onClick={changeColour}
-        style={{
-          backgroundColor: colours[counter],
-        }}
-      ></div>
-      <div className="square"></div>
-      <div className="square"></div>
+      <Trybox colours={colours} />
+      <Trybox colours={colours} />
+      <Trybox colours={colours} />
+      <Trybox colours={colours} />
     </div>
   );
 };
