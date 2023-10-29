d8097ae10bc061746fe062f8ada6757e8e0ccdaf
nikolaos-konstan <nikolaos.konstan@gmail.com>
2023-10-29 14:49:30 +0200

    compare works

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,28 @@ function App() {
 
   const submitCombo = (combo) => {
     console.log("the submitted combo is " + combo);
+    if (combo.length !== hiddenCombination.length) {
+      throw new Error("Arrays must be of the same size");
+    }
+
+    let countAll = 0; // To count elements appearing in both arrays regardless of index
+    let countSameIndex = 0; // To count elements appearing in the same index in both arrays
+
+    for (let i = 0; i < combo.length; i++) {
+      if (hiddenCombination.includes(combo[i])) {
+        countAll++;
+        if (combo[i] === hiddenCombination[i]) {
+          countSameIndex++;
+        }
+      }
+    }
+
+    console.log(countAll - countSameIndex);
+    console.log(countSameIndex);
+    // return {
+    //   countAll,
+    //   countSameIndex,
+    // };
   };
 
   return (
